a3bec3371d474cf5a108ae245335b7b09f717182
Yggdrasil09 <karthik99th.tnk@gmail.com>
2021-06-01 23:43:03 +0530

    Fixed the hamburger issue

--- src/Components/NavMenu/NavMenu.tsx
+++ src/Components/NavMenu/NavMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Navbar } from 'react-bootstrap';
 
@@ -12,6 +12,10 @@ interface InavLink {
 }
 
 function NavMenu(): JSX.Element {
+  const hamRef = useRef<HTMLButtonElement | null>(null);
+  const homeRef = useRef<HTMLAnchorElement | null>(null);
+  const itemsRef = useRef<HTMLAnchorElement[]>([]);
+
   return (
     <>
       <Navbar
@@ -21,13 +25,18 @@ function NavMenu(): JSX.Element {
         fixed="top"
         className="navBar"
       >
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" ref={hamRef} />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <NavLink className="mr-auto" to="/">
+          <NavLink ref={homeRef} className="mr-auto" to="/" onClick={() => { if (hamRef.current != null && window.innerWidth <= 991) hamRef.current.click(); }}>
             <NavButton name="Home" />
           </NavLink>
           {config.navLinks.map((obj: InavLink, index: number) => (
-            <NavLink key={obj.url} to={obj.url}>
+            <NavLink
+              key={obj.url}
+              ref={(element:HTMLAnchorElement) => itemsRef.current.push(element)}
+              to={obj.url}
+              onClick={() => { if (hamRef.current != null && window.innerWidth <= 991) hamRef.current.click(); }}
+            >
               <NavButton name={obj.name} index={index} />
             </NavLink>
           ))}
