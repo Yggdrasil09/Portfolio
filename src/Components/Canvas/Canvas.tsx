d3f13874ce5a5aab2410ebe616e2954ed6c4dde1
Yggdrasil09 <karthik99th.tnk@gmail.com>
2021-06-10 10:11:14 +0530

    Removed 3d text and added background

--- src/Components/Canvas/Canvas.tsx
+++ src/Components/Canvas/Canvas.tsx
@@ -2,13 +2,13 @@ import React, { useRef, useMemo, Suspense } from 'react';
 import * as THREE from 'three';
 import { Canvas, useFrame } from '@react-three/fiber';
 
-import JSONfont from '../../Utilities/Roboto_Regular.json';
 import './Canvas.scss';
 
-interface Props {
-    text: string
-    size: number
-    position: any
+interface dotProperties {
+  vec: THREE.Vector3,
+  transform: THREE.Matrix4,
+  positions: THREE.Vector3[],
+  distances: number[]
 }
 
 const roundedSquareWave = (
@@ -16,28 +16,69 @@ const roundedSquareWave = (
   delta: number,
   a: number,
   f: number,
-): number => (
-  ((2 * a) / Math.PI) * Math.atan(Math.sin(2 * Math.PI * t * f) / delta)
-);
+): number => ((2 * a) / Math.PI) * Math.atan(Math.sin(2 * Math.PI * t * f) / delta);
 
-function Text({ text, size = 1.5, position }: Props): JSX.Element {
-  const font = new THREE.FontLoader().parse(JSONfont);
+function getdotProperties(): dotProperties {
+  const vec = new THREE.Vector3();
+  const transform = new THREE.Matrix4();
 
-  const textOptions = {
-    font,
-    size: 5,
-    height: 1.5,
+  const positions = [...Array(10000)].map((_, i) => {
+    const position = new THREE.Vector3();
+
+    position.x = (i % 50) - 25;
+    position.y = Math.floor(i / 50) - 25;
+
+    position.y += (i % 2) * 0.5;
+
+    position.x += Math.random() * 0.3;
+    position.y += Math.random() * 0.3;
+
+    position.z = -10;
+    return position;
+  });
+
+  const right = new THREE.Vector3(1, 0, 0);
+  const distances = positions.map((pos) => pos.length() + Math.cos(pos.angleTo(right) * 4) * 0.5);
+  return {
+    vec, transform, positions, distances,
   };
+}
 
-  const mesh = useRef<THREE.Mesh | null>(null);
+function Dots(): JSX.Element {
+  const ref = useRef<THREE.InstancedMesh | null>(null);
+
+  const {
+    vec, transform, positions, distances,
+  }: dotProperties = useMemo(() => getdotProperties(), []);
+
+  useFrame(({ clock }) => {
+    for (let i = 0; i < 10000; i += 1) {
+      const dist = distances[i];
+
+      const t = clock.elapsedTime - dist / 5;
+
+      const wave = roundedSquareWave(
+        t,
+        0.15 + (0.2 * dist) / 72,
+        0.4,
+        1 / 3.8,
+      );
+
+      vec.copy(positions[i]).multiplyScalar(wave + 1.3);
+
+      transform.setPosition(vec);
+
+      if (ref.current !== null) ref.current.setMatrixAt(i, transform);
+    }
+    if (ref.current !== null) ref.current.instanceMatrix.needsUpdate = true;
+  });
 
   return (
-    <group position={position} scale={[0.1 * size, 0.1 * size, 0.3]}>
-      <mesh ref={mesh}>
-        <textGeometry attach="geometry" args={[text, textOptions]} />
-        <meshNormalMaterial attach="material" />
-      </mesh>
-    </group>
+    // eslint-disable-next-line
+    <instancedMesh ref={ref} args={[null as any, null as any, 10000]}>
+      <circleBufferGeometry args={[0.05]} />
+      <meshBasicMaterial color="#B4DFE5" />
+    </instancedMesh>
   );
 }
 
@@ -52,9 +93,7 @@ function CanvasC(): JSX.Element {
           angle={-4}
           castShadow
         />
-        <Text position={[0, 1, 0]} text="three.js" size={2} />
-        <Text position={[0, -1, 0]} text="three.js" size={2} />
-        <Text position={[0, -2, 0]} text="three.js" size={2} />
+        <Dots />
       </Suspense>
     </Canvas>
   );
